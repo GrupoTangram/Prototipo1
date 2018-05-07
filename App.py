cc176524b668c01dd42baa84e68724c5a9d586dd
Your Name <you@example.com>
2018-05-07 18:06:55 -0300

    Correção de bugs

--- App.py
+++ App.py
@@ -1,20 +1,19 @@
 from tkinter import *
 from tkinter import ttk
 
-import SerialHandler
-import json
+from SerialHandler import *
+
 
 data = ["a","b","c",]    
 
 labels = [(0,"Botão 1:"), (1,"Botão 2:"), (2,"Botão 3:")]
 
-sender = SerialHandler.SerialHandler()
+ser = serial.Serial("COM13",9600)
 
 class window:
     def __init__(self,tk):
-        self.arduinoStatusText = "Carrregando..."
         self.boxes = []
-
+        
         for index,a in labels :
             f = Frame(tk)
             f.pack()
@@ -26,39 +25,37 @@ class window:
             if(index == 2):
                 frameBottom = Frame(tk)
                 frameBottom.pack(pady=20)
-                self.arduinoStatusLabel = Label(frameBottom,text="Carregando...")
-                self.arduinoStatusLabel.pack(side="left")
-
+                
                 submitBtn = Button(frameBottom, text ="Confirmar", command = self.submit)
                 submitBtn.pack(padx=10,pady=10)
-        
-    
+
     def submit(self):  
-        keys = [] 
+        keys = []
         for a in self.boxes:
             keys.append(a.current())
-
-        try:
-            
-            sender.sendValues(keys)
-        except IOError:
-            self.arduinoStatusLabel['fg']='RED'
-           
+        for a in keys:
+            ser.write(struct.pack('>B', int(a)))
+            print(a)
+			      
 root = Tk()
 root.resizable(False, False)
 root.title("Tangram")
 root.geometry("300x300")
 label = Label( root, text="Mudar funções dos botões", font=("Helvetica", 16))
 label.pack(pady=10)
-app = window(root) 
+app = window(root)
 
 def update():
-
-    app.arduinoStatusLabel['text'] = sender.tryConnection()
-    app.arduinoStatusLabel['fg'] = 'BLACK'
-    root.after(2000, update)
+    global ser
+    try:
+        port = getPort()
+    except IOError:
+        print("Arduino não conectado!")
+        root.after(2000, update)
+    else:
+        print("conectou")
+        
 root.after(2000, update)
-
 root.mainloop()
 
 
--- SerialHandler.py
+++ SerialHandler.py
@@ -1,37 +1,31 @@
 import struct
-
 import serial
 import serial.tools.list_ports
 
-ser = serial.Serial("COM13",9600)
-
 class SerialHandler():
 	def __init__(self):
-		self.ARDUINO_STATUS = True
-		self.ser = None
-
-	def tryConnection(self):
-	
-		arduino_ports = [
-			p[0]
-			for p in serial.tools.list_ports.comports()
-			if 'Arduino' in p[1]
-		]
-		
-		if not arduino_ports:
-			self.ARDUINO_STATUS = False
-			return "Nenhum dispositivo conectado!"       
-		else:
-			self.ARDUINO_STATUS = True
-			self.ser = serial.Serial(arduino_ports[0], 9600)
-			return "Dispositivo conectado!"
-			
+		self.ser = serial.Serial()
+	def configureSerial(self,port):
+		self.ser.baudrate = 9600
+		self.ser.port = port
 
 	def sendValues(self,keys):		
-		if(self.ARDUINO_STATUS == True):
 			for a in keys:
-				ser.write(struct.pack('>B', int(a)))
+				self.ser.write(struct.pack('>B', int(a)))
 				print(a)
 			
-		else:
-			raise IOError("Nenhum Arduino encontrado")
+		
+def getPort():
+	arduino_ports = [
+		p[0]
+		for p in serial.tools.list_ports.comports()
+		if 'Arduino' in p[1]
+	]
+
+	if not arduino_ports:
+		raise IOError    
+	else:
+		return arduino_ports[0]
+	
+
+	
