3f5d1135a3651407243c04c9e8a6f61d4d421340
Arthurcrs <acrs@cesar.school>
2018-05-10 14:54:52 -0300

    add org fixes

--- App.py
+++ App.py
@@ -6,28 +6,30 @@ from SerialHandler import *
 numberOfButtons = 3 #Number of buttons
 
 #List that will contain the labels number and text. Ex: [(0,"Botão 1"),(1,"Botão 2")... ]
-labels = [(i,str("Botão " + str(i+1))) for i in range(numberOfButtons - 1)] 
+labels = [(i,str("Botão " + str(i+1))) for i in range(numberOfButtons )] 
 
 button_functions = ["Uber","Facebook","Twitter"] #Functions that the user can select for the buttons  
 
 class window:
     def __init__(self,tk):
         self.boxes = [] 
-        
-        for i,indexj in labels :
+
+        #This will loop making labels_number loop throgh the first element in labels ( the numbers )
+        #the second element will be the text "Botão .."
+        for labels_number,labels_text in labels :
 
             newFrame = Frame(tk)
             newFrame.pack()
 
-            newLabel = Label(newFrame,text=indexj)
+            newLabel = Label(newFrame,text=labels_text)
             newLabel.pack(side="left")
 
-            newCombobox = ttk.Combobox(newFrame,text=indexj,values=button_functions)
+            newCombobox = ttk.Combobox(newFrame,text=labels_text,values=button_functions)
             newCombobox.pack(padx=15,pady=10)
 
-            self.boxes.append(c)
+            self.boxes.append(newCombobox)
             
-            if i == numberOfButtons: #Create a the combobox after reaching the last button
+            if labels_number == numberOfButtons - 1: #Create a the combobox after reaching the last button
                 frameBottom = Frame(tk)
                 frameBottom.pack(pady=20)
                 submitBtn = Button(frameBottom, text ="Confirmar", command = self.submitFunction) #Create a button that executes the function submit when pressed
@@ -53,8 +55,8 @@ else: # If found an arduino in a port
     mainWindow.resizable(False, False) #Block the user to change the size of the main window
     mainWindow.title("Tangram") #Add title for the main window
     mainWindow.geometry("300x300") # Size of the main window
-    indexj = Label( mainWindow, text="Mudar funções dos botões", font=("Helvetica", 16)) 
-    indexj.pack(pady=10)
+    labels_text = Label( mainWindow, text="Mudar funções dos botões", font=("Helvetica", 16)) 
+    labels_text.pack(pady=10)
     app = window(mainWindow)
 
     mainWindow.mainloop() #Continue looping to keep the interface
