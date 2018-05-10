cd326475cda84d0ee1b91d9ad299781d3fe2db47
Arthurcrs <acrs@cesar.school>
2018-05-10 14:37:02 -0300

    Adding coments and organizing

--- App.py
+++ App.py
@@ -1,62 +1,60 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
-
 from SerialHandler import *
+ 
+numberOfButtons = 3 #Number of buttons
 
+#List that will contain the labels number and text. Ex: [(0,"Botão 1"),(1,"Botão 2")... ]
+labels = [(i,str("Botão " + str(i+1))) for i in range(numberOfButtons - 1)] 
 
-data = ["Uber","Facebook","Twitter"]    
-
-labels = [(0,"Botão 1:"), (1,"Botão 2:"), (2,"Botão 3:")]
-keys = []
+button_functions = ["Uber","Facebook","Twitter"] #Functions that the user can select for the buttons  
 
 class window:
     def __init__(self,tk):
         self.boxes = [] 
         
-        for index,label in labels :
+        for i,indexj in labels :
 
-            f = Frame(tk)
-            f.pack()
+            newFrame = Frame(tk)
+            newFrame.pack()
 
-            l = Label(f,text=label)
-            l.pack(side="left")
+            newLabel = Label(newFrame,text=indexj)
+            newLabel.pack(side="left")
 
-            c = ttk.Combobox(f,text=label,values=data)
-            c.pack(padx=15,pady=10)
+            newCombobox = ttk.Combobox(newFrame,text=indexj,values=button_functions)
+            newCombobox.pack(padx=15,pady=10)
+
             self.boxes.append(c)
-
-            if(index == 2):
+            
+            if i == numberOfButtons: #Create a the combobox after reaching the last button
                 frameBottom = Frame(tk)
                 frameBottom.pack(pady=20)
-                
-                submitBtn = Button(frameBottom, text ="Confirmar", command = self.submit)
+                submitBtn = Button(frameBottom, text ="Confirmar", command = self.submitFunction) #Create a button that executes the function submit when pressed
                 submitBtn.pack(padx=10,pady=10)
 
-    def submit(self):  
-        global keys 
+    def submitFunction(self):  #Function that is exectuted when a button is pressed
 
-        for a in self.boxes:
-            keys.append(a.current())
+        for i in self.boxes:
+            keys.append(i.current())
             
-        for a in keys:
-            ser.write(struct.pack('>B', int(a)))
-            print(a)
-
+        for i in keys:
+            ser.write(struct.pack('>B', int(i))) #Sent to the serial the number of the function
+            print(i)
 		      
-root = Tk()
-try:
-    ser = serial.Serial(getPort(),9600)
-except:
+mainWindow = Tk() # create main window
+ 
+try: #Try to connect with the arduino
+    ser = serial.Serial(getPort(),9600) #getPort will return [] if there is no arduino connected
+except: #When if fails to find an arduino connected
     messagebox.showwarning("Tangram","Nenhum arduino conectado!")
-else:
-    root.resizable(False, False)
-    root.title("Tangram")
-    root.geometry("300x300")
-    label = Label( root, text="Mudar funções dos botões", font=("Helvetica", 16))
-    label.pack(pady=10)
-    app = window(root)
+else: # If found an arduino in a port
+    
+    mainWindow.resizable(False, False) #Block the user to change the size of the main window
+    mainWindow.title("Tangram") #Add title for the main window
+    mainWindow.geometry("300x300") # Size of the main window
+    indexj = Label( mainWindow, text="Mudar funções dos botões", font=("Helvetica", 16)) 
+    indexj.pack(pady=10)
+    app = window(mainWindow)
 
-    root.mainloop()
-
-
+    mainWindow.mainloop() #Continue looping to keep the interface
